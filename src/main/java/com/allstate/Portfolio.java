d210e3f3864aae56e8e34df33c0c4c5a2cc72def
vinit <vinit.rohela@gmail.com>
2017-01-02 16:20:28 +0530

    change

--- src/main/java/com/allstate/Portfolio.java
+++ src/main/java/com/allstate/Portfolio.java
@@ -3,8 +3,12 @@ package com.allstate;
 
 public class Portfolio {
     protected String name;
+    protected 
 
     public Portfolio(String name){
         this.name = name;
+    }
+    public String addStock(int stock){
+        this.stock += stock
     }
 }
